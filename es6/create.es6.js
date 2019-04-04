c388614d95c155594561b76a2f792f40062d21a6
ahabhgk <ahabhgk@gmail.com>
2019-04-04 13:18:59 +0800

    🌟oop2

--- es6/create.es6.js
+++ es6/create.es6.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 
 class Parent {
+  // constructor方法默认返回实例对象（即this），完全可以指定返回另外一个对象
   constructor(name) {
     this.name = name
   }
 
+  // 静态方法不会被实例继承，而是直接通过类来调用：Parent.getAge()
   static getAge() {
     console.log('no age, haha')
   }
--- es6/inherits.es6.js
+++ es6/inherits.es6.js
@@ -19,7 +19,7 @@ class Parent {
 
 class Child extends Parent {
   constructor(name, age) {
-    // super 指父类的构造函数
+    // super 代表父类的构造函数，用来新建父类的this对象
     super(name)
     this.age = age
   }
@@ -28,6 +28,13 @@ class Child extends Parent {
     console.log(this.age)
   }
 }
+
+/**
+ * ES5 的继承，实质是先创造子类的实例对象this，然后再将父类的方法添加到this上面（Parent.apply(this)）
+ * ES6 的继承机制完全不同，实质是先将父类实例对象的属性和方法，加到this上面（所以必须先调用super方法），然后再用子类的构造函数修改this
+ *
+ * ES6 允许继承原生构造函数定义子类，因为 ES6 是先新建父类的实例对象this，然后再用子类的构造函数修饰this，使得父类的所有行为都可以继承
+ */
 
 
 // new 干了什么？new 的模拟
